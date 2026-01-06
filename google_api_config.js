3d1e849f7755a565cd3f6d1b1ad2add13ccc62ce
ANDY <tpc0941101090@gmail.com>
2026-01-06 15:28:22 +0800

    Config: Update Production API Endpoint

--- google_api_config.js
+++ google_api_config.js
@@ -1,3 +1,4 @@
 ﻿window.GOOGLE_API_CONFIG = {
-    apiUrl: "https://script.google.com/macros/s/AKfycbxQmktoDnkf6HXEbIAL8tcfKDt3n9bvuDyOuYL_pbZYDgG7w1gfhiqEoZnN9BYhHVKu/exec"
+    apiUrl: "https://script.google.com/macros/s/AKfycbz0I1XUI2QDpdO6Gaf40wZ62zcxE82DOkGTryJDyeriPxNbAbYYFOfTTmWVZN7sJGr-/exec", // 您的 API URL
+    apiToken: "CALENDAR_CARD_APP_2026", // 自訂的驗證金鑰，需與後端一致8tcfKDt3n9bvuDyOuYL_pbZYDgG7w1gfhiqEoZnN9BYhHVKu/exec"
 };
