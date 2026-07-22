3c69d851b0b90617c98c618585ae7921d8b7f255
ANDY <tpc0941101090@gmail.com>
2026-07-22 13:41:58 +0800

    Fix: 移除 google_api_config.js 等敏感檔案的線上追蹤

--- google_api_config.js
+++
@@ -1,4 +0,0 @@
-window.GOOGLE_API_CONFIG = {
-    apiUrl: "https://script.google.com/macros/s/AKfycbyhcipT8h3pWEN27-wl4gjYOTxOoTHQOSOiv9SOp6U8SGlMzHheshsRBRZF-YwsIno9/exec", // 您的新 API URL
-    apiToken: "SECURE_TOKEN_5X9Y2Z_2026", // 新的驗證金鑰
-};
